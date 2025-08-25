f8d9056306a6ea920e5b13ece9f0537acb38881e
Oduala Oluwatosin <oluwatosinoduala@gmail.com>
2025-08-25 15:05:36 +0100

    feat: building routes for the navigation pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointment from './pages/MyAppointment'
+import Appoinment from './pages/Appointment'
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+
+      <Routes>
+        <Route path='/' element= {<Home />}/>
+        <Route path='/doctors' element= {<Doctors />}/>
+        <Route path='/doctors/:specialty' element= {<Doctors />}/>
+        <Route path='/login' element= {<Login />}/>
+        <Route path='/about' element= {<About />}/>
+        <Route path='/contact' element= {<Contact />}/>
+        <Route path='/my-profile' element= {<MyProfile />}/>
+        <Route path='/my-appointment' element= {<MyAppointment />}/>
+        <Route path='/appointment/:docsId' element= {<Appointment />}/>
+        
+
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
